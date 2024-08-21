040252c6c734310e9b8329da9080f8f3afffc8bc
AugustinDmrt <55685689+AugustinDmrt@users.noreply.github.com>
2024-08-21 17:49:49 +0200

    fin des test

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -1,85 +1,286 @@
-import { fireEvent, screen, waitFor } from "@testing-library/dom";
-import userEvent from "@testing-library/user-event";
-import mockStore from "../__mocks__/store";
-import router from "../app/Router";
-import { ROUTES, ROUTES_PATH } from "../constants/routes";
-import NewBill from "../containers/NewBill";
+/**
+ * @jest-environment jsdom
+ */
 
-jest.mock("../app/store", () => mockStore);
+import { fireEvent, screen, waitFor } from "@testing-library/dom";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store";
+import router from "../app/Router.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes";
+import NewBill from "../containers/NewBill.js";
+import NewBillUI from "../views/NewBillUI.js";
 
 describe("Given I am connected as an employee", () => {
-  beforeEach(() => {
-    window.localStorage.setItem(
-      "user",
-      JSON.stringify({ type: "Employee", email: "employee@test.com" })
-    );
+  describe("When I am on NewBill page, there are a mail icon in vertical layout", () => {
+    test("Then, the icon should be highlighted", async () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.NewBill);
+      await waitFor(() => screen.getByTestId("icon-mail"));
+      const windowIcon = screen.getByTestId("icon-mail");
+      const isIconActivated = windowIcon.classList.contains("active-icon");
+      expect(isIconActivated).toBeTruthy();
+    });
+  });
+  describe("Then I am on NewBill page, there are a form", () => {
+    test("Then, all the form input should be render correctly", () => {
+      document.body.innerHTML = NewBillUI();
 
-    const root = document.createElement("div");
-    root.setAttribute("id", "root");
-    document.body.append(root);
-    router();
-    window.onNavigate(ROUTES_PATH.NewBill);
+      const formNewBill = screen.getByTestId("form-new-bill");
+      const type = screen.getAllByTestId("expense-type");
+      const name = screen.getAllByTestId("expense-name");
+      const date = screen.getAllByTestId("datepicker");
+      const amount = screen.getAllByTestId("amount");
+      const vat = screen.getAllByTestId("vat");
+      const pct = screen.getAllByTestId("pct");
+      const commentary = screen.getAllByTestId("commentary");
+      const file = screen.getAllByTestId("file");
+      const submitBtn = document.querySelector("#btn-send-bill");
+
+      expect(formNewBill).toBeTruthy();
+      expect(type).toBeTruthy();
+      expect(name).toBeTruthy();
+      expect(date).toBeTruthy();
+      expect(amount).toBeTruthy();
+      expect(vat).toBeTruthy();
+      expect(pct).toBeTruthy();
+      expect(commentary).toBeTruthy();
+      expect(file).toBeTruthy();
+      expect(submitBtn).toBeTruthy();
+
+      expect(screen.getAllByText("Envoyer une note de frais")).toBeTruthy();
+    });
+  });
+  describe("When I am on NewBill page, and a user upload a accepted format file", () => {
+    test("Then, the file name should be correctly displayed into the input and isImgFormatValid shoud be true", () => {
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+
+      document.body.innerHTML = NewBillUI();
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      const store = null;
+
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        store,
+        localStorage,
+      });
+      const handleChangeFile = jest.fn(() => newBill.handleChangeFile);
+      const file = screen.getByTestId("file");
+
+      window.alert = jest.fn();
+
+      file.addEventListener("change", handleChangeFile);
+      fireEvent.change(file, {
+        target: {
+          files: [new File(["file.png"], "file.png", { type: "image/png" })],
+        },
+      });
+
+      jest.spyOn(window, "alert");
+      expect(alert).not.toHaveBeenCalled();
+
+      expect(handleChangeFile).toHaveBeenCalled();
+      expect(file.files[0].name).toBe("file.png");
+      expect(newBill.fileName).toBe("file.png");
+      expect(newBill.isImgFormatValid).toBe(true);
+      expect(newBill.formData).not.toBe(null);
+    });
+  });
+  describe("When I am on NewBill page, and a user upload a unaccepted format file", () => {
+    test("Then, the file name should not be displayed into the input, isImgFormatValid shoud be false and a alert should be displayed", () => {
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+
+      document.body.innerHTML = NewBillUI();
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      const store = null;
+
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        store,
+        localStorage,
+      });
+      const handleChangeFile = jest.fn(newBill.handleChangeFile);
+      const file = screen.getByTestId("file");
+
+      window.alert = jest.fn();
+
+      file.addEventListener("change", handleChangeFile);
+      fireEvent.change(file, {
+        target: {
+          files: [new File(["file.pdf"], "file.pdf", { type: "file/pdf" })],
+        },
+      });
+
+      jest.spyOn(window, "alert");
+      expect(alert).toHaveBeenCalled();
+
+      expect(handleChangeFile).toHaveBeenCalled();
+      expect(newBill.fileName).toBe(null);
+      expect(newBill.isImgFormatValid).toBe(false);
+      expect(newBill.formData).toBe(undefined);
+    });
   });
 
-  describe("When I submit a new Bill", () => {
-    test("Then verify the file bill", async () => {
-      const newBillInit = new NewBill({
+  describe("When I am on NewBill page, and the user click on submit button", () => {
+    test("Then, the handleSubmit function should be called", () => {
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+
+      document.body.innerHTML = NewBillUI();
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+      const store = {
+        bills: jest.fn(() => newBill.store),
+        create: jest.fn(() => Promise.resolve({})),
+      };
+
+      const newBill = new NewBill({
         document,
-        onNavigate: (pathname) => {
-          document.body.innerHTML = ROUTES({ pathname });
-        },
-        store: mockStore,
-        localStorage: window.localStorage,
+        onNavigate,
+        store,
+        localStorage,
       });
 
-      await waitFor(() => {
-        expect(screen.getByTestId("form-new-bill")).toBeTruthy();
-      });
+      newBill.isImgFormatValid = true;
 
-      const billFileInput = screen.getByTestId("file");
-      const file = new File(["image"], "image.png", { type: "image/png" });
-      userEvent.upload(billFileInput, file);
-
-      const handleSubmit = jest.fn(newBillInit.handleSubmit);
       const formNewBill = screen.getByTestId("form-new-bill");
+      const handleSubmit = jest.fn(newBill.handleSubmit);
       formNewBill.addEventListener("submit", handleSubmit);
       fireEvent.submit(formNewBill);
 
       expect(handleSubmit).toHaveBeenCalled();
     });
   });
+});
 
-  describe("When I can't upload an invalid file", () => {
-    test("Then show an error message", async () => {
-      const newBillInit = new NewBill({
-        document,
-        onNavigate: (pathname) => {
+//POST
+describe("When I navigate to Dashboard employee", () => {
+  describe("Given I am a user connected as Employee, and a user post a newBill", () => {
+    test("Add a bill from mock API POST", async () => {
+      const postSpy = jest.spyOn(mockStore, "bills");
+      const bill = {
+        id: "47qAXb6fIm2zOKkLzMro",
+        vat: "80",
+        fileUrl:
+          "https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
+        status: "pending",
+        type: "Hôtel et logement",
+        commentary: "séminaire billed",
+        name: "encore",
+        fileName: "preview-facture-free-201801-pdf-1.jpg",
+        date: "2004-04-04",
+        amount: 400,
+        commentAdmin: "ok",
+        email: "a@a",
+        pct: 20,
+      };
+      const postBills = await mockStore.bills().update(bill);
+      expect(postSpy).toHaveBeenCalledTimes(1);
+      expect(postBills).toStrictEqual(bill);
+    });
+    describe("When an error occurs on API", () => {
+      beforeEach(() => {
+        window.localStorage.setItem(
+          "user",
+          JSON.stringify({
+            type: "Employee",
+          })
+        );
+
+        document.body.innerHTML = NewBillUI();
+
+        const onNavigate = (pathname) => {
           document.body.innerHTML = ROUTES({ pathname });
-        },
-        store: mockStore,
-        localStorage: window.localStorage,
+        };
       });
+      test("Add bills from an API and fails with 404 message error", async () => {
+        const postSpy = jest.spyOn(console, "error");
 
-      await waitFor(() => {
-        expect(screen.getByTestId("form-new-bill")).toBeTruthy();
+        const store = {
+          bills: jest.fn(() => newBill.store),
+          create: jest.fn(() => Promise.resolve({})),
+          update: jest.fn(() => Promise.reject(new Error("404"))),
+        };
+
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          store,
+          localStorage,
+        });
+        newBill.isImgFormatValid = true;
+
+        // Submit form
+        const form = screen.getByTestId("form-new-bill");
+        const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
+        form.addEventListener("submit", handleSubmit);
+
+        fireEvent.submit(form);
+        await new Promise(process.nextTick);
+        expect(postSpy).toBeCalledWith(new Error("404"));
       });
+      test("Add bills from an API and fails with 500 message error", async () => {
+        const postSpy = jest.spyOn(console, "error");
 
-      const billFileInput = screen.getByTestId("file");
+        const store = {
+          bills: jest.fn(() => newBill.store),
+          create: jest.fn(() => Promise.resolve({})),
+          update: jest.fn(() => Promise.reject(new Error("500"))),
+        };
 
-      // Ajoutez un console.log pour voir si l'élément est bien sélectionné
-      console.log(billFileInput);
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          store,
+          localStorage,
+        });
+        newBill.isImgFormatValid = true;
 
-      const invalidFile = new File(["document"], "document.pdf", {
-        type: "application/pdf",
+        // Submit form
+        const form = screen.getByTestId("form-new-bill");
+        const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
+        form.addEventListener("submit", handleSubmit);
+
+        fireEvent.submit(form);
+        await new Promise(process.nextTick);
+        expect(postSpy).toBeCalledWith(new Error("500"));
       });
-      userEvent.upload(billFileInput, invalidFile);
-
-      await waitFor(() => expect(billFileInput.value).toBe(""));
-
-      // Vérifiez que le message d'erreur s'affiche
-      expect(
-        screen.queryByText("Seuls les fichiers jpg, jpeg et png sont acceptés.")
-      ).toBeTruthy();
     });
   });
 });
--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -6,69 +6,64 @@ export default class NewBill {
     this.document = document;
     this.onNavigate = onNavigate;
     this.store = store;
-    this.localStorage = localStorage || window.localStorage; // Utilisation du localStorage simulé si nécessaire
-
     const formNewBill = this.document.querySelector(
       `form[data-testid="form-new-bill"]`
     );
     formNewBill.addEventListener("submit", this.handleSubmit);
-
     const file = this.document.querySelector(`input[data-testid="file"]`);
     file.addEventListener("change", this.handleChangeFile);
-
     this.fileUrl = null;
     this.fileName = null;
     this.billId = null;
-
-    new Logout({ document, localStorage: this.localStorage, onNavigate });
+    new Logout({ document, localStorage, onNavigate });
   }
-
   handleChangeFile = (e) => {
     e.preventDefault();
     const file = this.document.querySelector(`input[data-testid="file"]`)
       .files[0];
-    const filePath = e.target.value.split(/\\/g);
-    const fileName = filePath[filePath.length - 1];
-    const fileExtension = fileName.split(".").pop().toLowerCase();
-    const acceptedExtensions = ["jpg", "jpeg", "png"];
-
-    if (!acceptedExtensions.includes(fileExtension)) {
-      alert("Seuls les fichiers jpg, jpeg et png sont acceptés.");
-      console.error("wrong extension");
-      e.target.value = "";
-      return;
+    const fileName = file.name;
+    // new variables needed to check image format
+    const fileInput = this.document.querySelector(`input[data-testid="file"]`);
+    const fileAcceptedFormats = ["jpg", "jpeg", "png"];
+    const fileNameParts = fileName.split(".");
+    const fileExtension = fileNameParts[fileNameParts.length - 1].toLowerCase();
+    this.isImgFormatValid = false;
+    // if there are at least two pieces to the file name, continue the check
+    if (fileNameParts.length > 1) {
+      // Check if image format is .jpg, jpeg or png - if true set isImgFormatValid to true else false
+      fileAcceptedFormats.indexOf(fileExtension) !== -1
+        ? (this.isImgFormatValid = true)
+        : (this.isImgFormatValid = false);
     }
-
-    const formData = new FormData();
-    const email = JSON.parse(this.localStorage.getItem("user")).email;
-    formData.append("file", file);
-    formData.append("email", email);
-
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true,
-        },
-      })
-      .then(({ fileUrl, key }) => {
-        console.log(fileUrl);
-        this.billId = key;
-        this.fileUrl = fileUrl;
-        this.fileName = fileName;
-      })
-      .catch((error) => console.error(error));
+    if (!this.isImgFormatValid) {
+      // if image format is not valid ...
+      fileInput.value = ""; // ... remove file from the input
+      fileInput.classList.add("is-invalid"); // ... add is-invalid class to tell user input is invalid
+      fileInput.classList.remove("blue-border"); // ... remove blue-border class
+      alert(
+        "Le format de votre fichier n'est pas pris en charge." +
+          "\n" +
+          "Seuls les .jpg, .jpeg, .png sont acceptés."
+      ); // Error message for user
+    } else {
+      // if image format is valid ...
+      fileInput.classList.remove("is-invalid"); // ... remove is-invalid class
+      fileInput.classList.add("blue-border"); // ... add blue-border class
+      const formData = new FormData();
+      const email = JSON.parse(localStorage.getItem("user")).email;
+      formData.append("file", file);
+      formData.append("email", email);
+      this.formData = formData; // so it can be used in other methods
+      this.fileName = fileName;
+    }
   };
-
   handleSubmit = (e) => {
     e.preventDefault();
     console.log(
       'e.target.querySelector(`input[data-testid="datepicker"]`).value',
       e.target.querySelector(`input[data-testid="datepicker"]`).value
     );
-
-    const email = JSON.parse(this.localStorage.getItem("user")).email;
+    const email = JSON.parse(localStorage.getItem("user")).email;
     const bill = {
       email,
       type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
@@ -87,11 +82,31 @@ export default class NewBill {
       fileName: this.fileName,
       status: "pending",
     };
-    this.updateBill(bill);
-    this.onNavigate(ROUTES_PATH["Bills"]);
+    if (this.isImgFormatValid) {
+      // if image format is valid ...
+      // ... move in handleSubmit to upload image and create new bill only when image format is valid and form is complete
+      this.store
+        .bills()
+        .create({
+          data: this.formData,
+          headers: {
+            noContentType: true,
+          },
+        })
+        .then(({ fileUrl, key }) => {
+          console.log(fileUrl);
+          this.billId = key;
+          this.fileUrl = fileUrl;
+        })
+        .then(() => {
+          this.updateBill(bill);
+        })
+        .catch((error) => console.error(error));
+    }
   };
 
-  // Not covered by tests
+  // not need to cover this function by tests
+  /* istanbul ignore next */
   updateBill = (bill) => {
     if (this.store) {
       this.store
